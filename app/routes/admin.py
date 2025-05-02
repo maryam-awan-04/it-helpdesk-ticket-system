0484f56183a53ea29caae76a1d28b80c924b1873
Maryam Awan <maryamawan04@gmail.com>
2025-05-02 22:58:57 +0100

    Admin manage users interface

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -4,6 +4,7 @@ Admin routes for the application
 
 from flask import Blueprint, redirect, render_template, url_for
 
+from app.forms.registration import RegistrationForm
 from app.forms.ticket import Ticket
 
 bp = Blueprint("admin", __name__, url_prefix="/admin")
@@ -23,3 +24,14 @@ def manage_tickets():
         return redirect(url_for("admin.dashboard"))
 
     return render_template("admin/manage_tickets.html", form=form)
+
+
+@bp.route("/manage-users", methods=["GET", "POST"])
+def manage_users():
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        return redirect(url_for("admin.dashboard"))
+
+    return render_template("admin/manage_users.html", form=form)
