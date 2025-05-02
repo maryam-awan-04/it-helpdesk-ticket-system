a7d9490f1b721d9f9bac7205ee879dbff5b02ce7
Maryam Awan <maryamawan04@gmail.com>
2025-05-02 20:45:37 +0100

    Admin manage tickets interface

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -2,7 +2,9 @@
 Admin routes for the application
 """
 
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
+
+from app.forms.ticket import Ticket
 
 bp = Blueprint("admin", __name__, url_prefix="/admin")
 
@@ -10,3 +12,14 @@ bp = Blueprint("admin", __name__, url_prefix="/admin")
 @bp.route("/dashboard", methods=["GET", "POST"])
 def dashboard():
     return render_template("admin/dashboard.html")
+
+
+@bp.route("/manage-tickets", methods=["GET", "POST"])
+def manage_tickets():
+
+    form = Ticket()
+
+    if form.validate_on_submit():
+        return redirect(url_for("admin.dashboard"))
+
+    return render_template("admin/manage_tickets.html", form=form)
